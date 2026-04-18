ba3a3901551c630a440fb4c5727e1ed7f5bdb189
Barathkumar S <barathshs@gmail.com>
2026-04-18 18:47:19 +0530

    done

--- backend/app.py
+++ backend/app.py
@@ -168,7 +168,7 @@ def calculate_water(crop, temp, humidity, rainfall, soil_moisture, area, flow):
 # ================= ROUTES =================
 @app.route('/')
 def home():
-    return "Backend Running 🚀"
+    return render_template('index.html')
 
 
 # ================= CROP RECOMMENDATION =================
